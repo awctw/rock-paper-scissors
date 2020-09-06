1c434ccc2c2819c0d24c4b4209584f5ff24ba5b3
Annabelle <awctw160@gmail.com>
2020-09-05 21:39:38 -0700

    Added simple ui with buttons and made results displayable

--- js/index.js
+++ js/index.js
@@ -6,98 +6,113 @@ function computerPlay() {
   return arr[i];
 }
 
+let div = document.querySelector("#results");
+
+//results for player selection of rock
+function rock(computerSelection) {
+  if (computerSelection === "Paper") {
+    div.innerHTML = "You Lose! Paper beats Rock";
+    currentScore("Lose");
+  } else if (computerSelection === "Scissors") {
+    div.innerHTML = "You Win! Rock beats Scissors";
+    currentScore("Win");
+  } else if (computerSelection === "Rock") {
+    div.innerHTML = "It's a Tie!";
+    currentScore("Tie");
+  }
+}
+
+//results for player selection of paper
+function paper(computerSelection) {
+  if (computerSelection === "Paper") {
+    div.innerHTML = "It's a Tie!";
+    currentScore("Tie");
+  } else if (computerSelection === "Scissors") {
+    div.innerHTML = "You Lose! Scissors beats Paper";
+    currentScore("Lose");
+  } else if (computerSelection === "Rock") {
+    div.innerHTML = "You Win! Paper beats Rock";
+    currentScore("Win");
+  }
+}
+
+//results for player selection of scissors
+function scissors(computerSelection) {
+  if (computerSelection === "Paper") {
+    div.innerHTML = "You Win! Scissors beats Paper";
+    currentScore("Win");
+  } else if (computerSelection === "Scissors") {
+    div.innerHTML = "It's a Tie!";
+    currentScore("Tie");
+  } else if (computerSelection === "Rock") {
+    div.innerHTML = "You Lose! Rock beats Scissors";
+    currentScore("Lose");
+  }
+}
+
 //calculates the winner between the player and computer
 function playRound(playerSelection, computerSelection) {
   let temp = playerSelection.toUpperCase();
-
-  switch (temp) {
-    case "ROCK":
-      if (computerSelection === "Paper") {
-        return "You Lose! Paper beats Rock";
-      } else if (computerSelection === "Scissors") {
-        return "You Win! Rock beats Scissors";
-      } else if (computerSelection === "Rock") {
-        return "It's a Tie!";
-      }
-
-    case "PAPER":
-      if (computerSelection === "Paper") {
-        return "It's a Tie!";
-      } else if (computerSelection === "Scissors") {
-        return "You Lose! Scissors beats Paper";
-      } else if (computerSelection === "Rock") {
-        return "You Win! Paper beats Rock";
-      }
-
-    case "SCISSORS":
-      if (computerSelection === "Paper") {
-        return "You Win! Scissors beats Paper";
-      } else if (computerSelection === "Scissors") {
-        return "It's a Tie!";
-      } else if (computerSelection === "Rock") {
-        return "You Lose! Rock beats Scissors";
-      }
-
-    default:
-      return "Your input is invalid!";
+  if (temp === "ROCK") {
+    rock(computerSelection);
+  } else if (temp === "PAPER") {
+    paper(computerSelection);
+  } else if (temp === "SCISSORS") {
+    scissors(computerSelection);
   }
 }
 
-//plays the game 5 times, records the current score
-function game() {
-  let computerScore = 0;
-  let userScore = 0;
+let computerScore = 0;
+let userScore = 0;
+let score = document.querySelector("#score");
+let final = document.querySelector("#finalScore");
 
-  for (let i = 1; i <= 5; i++) {
-    let input = prompt("Rock, Paper, Scissors?", "");
-
-    let result = playRound(input, computerPlay());
-
-    if (result.includes("Win")) {
-      userScore += 1;
-    } else if (result.includes("Lose")) {
-      computerScore += 1;
-    } else if (result.includes("Tie")) {
-      userScore += 1;
-      computerScore += 1;
-    }
-
-    alert(
-      result +
-        "\r\n" +
-        "The Current Score is: You = " +
-        userScore +
-        " Computer = " +
-        computerScore
-    );
+//displays the current score and the final winner
+function currentScore(result) {
+  if (result === "Win") {
+    userScore += 1;
+  } else if (result === "Lose") {
+    computerScore += 1;
+  } else if (result === "Tie") {
+    userScore += 1;
+    computerScore += 1;
   }
 
-  if (computerScore > userScore) {
-    alert(
+  score.innerHTML =
+    "The Current Score is: You = " + userScore + " Computer = " + computerScore;
+
+  if (computerScore === 5 && userScore === 5) {
+    "The Final Score is: You = " +
+      userScore +
+      " Computer = " +
+      computerScore +
+      "\r\n" +
+      "It's a Tie!";
+  } else if (computerScore === 5) {
+    final.innerHTML =
       "The Final Score is: You = " +
-        userScore +
-        " Computer = " +
-        computerScore +
-        "\r\n" +
-        "You Lost!"
-    );
-  } else if (computerScore < userScore) {
-    alert(
+      userScore +
+      " Computer = " +
+      computerScore +
+      "\r\n" +
+      "You Lost!";
+  } else if (userScore === 5) {
+    final.innerHTML =
       "The Final Score is: You = " +
-        userScore +
-        " Computer = " +
-        computerScore +
-        "\r\n" +
-        "You Won!"
-    );
-  } else if (computerScore === userScore) {
-    alert(
-      "The Final Score is: You = " +
-        userScore +
-        " Computer = " +
-        computerScore +
-        "\r\n" +
-        "It's a Tie!"
-    );
+      userScore +
+      " Computer = " +
+      computerScore +
+      "\r\n" +
+      "You Won!";
   }
 }
+
+const buttons = document.querySelectorAll("button");
+
+//makes each button the player selection and begin the game
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.id);
+    playRound(button.id, computerPlay());
+  });
+});
